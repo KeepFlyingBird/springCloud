0d1e1a7e5c10858e5b1e97b49a9f749c06fabde5
xiaoyunfei <xiaoyunfei@credit-first.com.cn>
2020-06-19 18:49:51 +0800

    提交删除无用包

--- src/main/java/cn/freeFly/springcloud/config/Test.java
+++
@@ -1,4 +0,0 @@
-package cn.freeFly.springcloud.config;
-
-public class Test {
-}
---
+++ src/main/java/cn/freeFly/springcloud/schedule/TimmerTaskSchedulerManage.java
@@ -0,0 +1,8 @@
+package cn.freeFly.springcloud.schedule;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimmerTaskSchedulerManage {
+
+}
