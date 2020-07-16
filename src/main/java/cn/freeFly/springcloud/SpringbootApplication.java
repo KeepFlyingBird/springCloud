b54892991002ae5a1c37bec79d06d5a3112827eb
xiaoyunfei <1226781445@qq.com>
2020-07-16 14:20:08 +0800

    consul

--- src/main/java/cn/freeFly/springcloud/SpringbootApplication.java
+++ src/main/java/cn/freeFly/springcloud/SpringbootApplication.java
@@ -3,6 +3,8 @@ package cn.freeFly.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -10,11 +12,16 @@ import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//feign
 @EnableFeignClients
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,value = {cn.freeFly.springcloud.annotationInterface.FeignExcludeComponent.class}))
+//feign
 @EnableSwagger2 //添加swagger启用注解
 @MapperScan("cn.freeFly.springcloud.mapper")//mapper文件包扫描
 @PropertySource(value = "classpath:rabbitMq.properties") //mq配置文件
+//consul
+@EnableDiscoveryClient
+//consul
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
---
+++ src/main/java/cn/freeFly/springcloud/controller/ConsulController.java
@@ -0,0 +1,46 @@
+package cn.freeFly.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class ConsulController {
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 获取所有服务
+     */
+    @RequestMapping("/api/discoveryClient")
+    public Object discover() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("consul-producer");
+        return instances;
+    }
+
+    /**
+     * 从所有服务中选择一个服务（轮询）
+     */
+    @RequestMapping("/api/loadBalancer")
+    public Object loadBalancer() {
+        ServiceInstance choose = loadBalancer.choose("consul-producer");
+        URI uri = choose.getUri();
+        return uri.toString();
+    }
+
+    @RequestMapping("/api/consulTest")
+    public String consulTest() {
+        return "hello,springCloud";
+    }
+
+
+
+}
